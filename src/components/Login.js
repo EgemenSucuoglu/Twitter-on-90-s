455e5a2605fe3212ed5b0cf1e160b0ee002821a5
Salim Çobanoğlu <salimcobanoglu@yahoo.com>
2023-05-09 09:50:33 +0200

    ekleme ve cikarma eklendi. overflow kaldi

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,44 +1,55 @@
-import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
-  const [email, setEmail] = useState();
-  const [pass, setPass] = useState();
+  const { handleSubmit, register } = useForm({});
+
+  let history = useHistory();
+
+  function gonder(data) {
+    axios
+      .post("https://wit-courses-api2.onrender.com/login", data)
+      .then(history.push("/homepage"))
+      .catch((err) => console.log(err));
+  }
 
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-6 mx-auto">
-          <div className="card">
-            <div className="card-header bg-info">
-              <h2>Login</h2>
-            </div>
-            <div className="card-body">
-              <form method="post">
-                <div className="form-group">
-                  <label for="Email"></label>
-                  <input
-                    value={email}
-                    type="email"
-                    className="form-control"
-                    name="email"
-                    placeholder="Email Adresinizi Giriniz"
-                  ></input>
-                </div>
-                <div className="form-group">
-                  <label for="Şifre"></label>
-                  <input
-                    value={pass}
-                    type="password"
-                    className="form-control"
-                    name="sifre"
-                    placeholder="Şifrenizi Giriniz"
-                  ></input>
-                </div>
-              </form>
-            </div>
+    <form onSubmit={handleSubmit(gonder)}>
+      <div className="container mt-5 col-6 mx-auto">
+        <div className="card">
+          <div className="card-header bg-info">
+            <h2>Login</h2>
+          </div>
+          <div className="card-body">
+            <form method="post">
+              <div className="form-group">
+                <label for="Email"></label>
+                <input
+                  {...register("email")}
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  placeholder="Email Adresinizi Giriniz"
+                ></input>
+              </div>
+              <div className="form-group">
+                <label for="Şifre"></label>
+                <input
+                  {...register("password")}
+                  type="password"
+                  className="form-control"
+                  name="sifre"
+                  placeholder="Şifrenizi Giriniz"
+                ></input>
+              </div>
+            </form>
+          </div>
+          <div>
+            <button> Gonder </button>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
